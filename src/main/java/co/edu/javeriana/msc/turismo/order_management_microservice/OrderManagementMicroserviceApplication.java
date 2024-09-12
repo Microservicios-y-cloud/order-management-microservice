045f9194efa6b15d62068f73a7e4d3dd1a753ec8
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-09-12 15:04:34 -0500

    first commit

---
+++ src/main/java/co/edu/javeriana/msc/turismo/order_management_microservice/OrderManagementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.msc.turismo.order_management_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementMicroserviceApplication.class, args);
+	}
+
+}
