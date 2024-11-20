091754223fe07dd87b7e2e89905cfc172b1f8e14
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2024-11-20 08:59:57 -0500

    Test done

--- src/test/java/co/edu/javeriana/msc/turismo/order_management_microservice/cart/controller/CartControllerTest.java
+++ src/test/java/co/edu/javeriana/msc/turismo/order_management_microservice/cart/controller/CartControllerTest.java
@@ -2,105 +2,210 @@ package co.edu.javeriana.msc.turismo.order_management_microservice.cart.controll
 
 import co.edu.javeriana.msc.turismo.order_management_microservice.cart.dtos.CartRequest;
 import co.edu.javeriana.msc.turismo.order_management_microservice.cart.dtos.CartResponse;
+import co.edu.javeriana.msc.turismo.order_management_microservice.cart.model.Cart;
 import co.edu.javeriana.msc.turismo.order_management_microservice.cart.model.CartItem;
+import co.edu.javeriana.msc.turismo.order_management_microservice.cart.repository.CartRepository;
+import co.edu.javeriana.msc.turismo.order_management_microservice.cart.services.CartService;
 import co.edu.javeriana.msc.turismo.order_management_microservice.dto.Customer;
 import co.edu.javeriana.msc.turismo.order_management_microservice.orders.dto.OrderPurchaseRequest;
-import org.junit.jupiter.api.Test;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
-
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import java.time.LocalDateTime;
-import java.util.List;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureGraphQlTester
+@Testcontainers
+@TestMethodOrder(OrderAnnotation.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CartControllerTest {
 
     @Autowired
-    private CartController cartController;
+    private CartService cartService;
 
-    private CartRequest createSampleCartRequest() {
-        LocalDateTime now = LocalDateTime.now();
-        Customer customer = new Customer("122", "customer", "Juan", "Diego", "Echeverry", "juan_echeverry@javeriana.edu.co");
-        List<CartItem> items = List.of(new CartItem(1L, 2, 100.0));
-        return new CartRequest("carrito", now, now, customer, items);
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Container
+    static final MongoDBContainer mongo = new MongoDBContainer("mongo:4.4.6");
+
+    @Container
+    static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.6.1"));
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.host", mongo::getHost);
+        registry.add("spring.data.mongodb.port", mongo::getFirstMappedPort);
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
+
+    static KafkaConsumer<Object, Object> mockKafkaConsumer;
+
+    static final Cart carrito1 = Cart.builder()
+            .id("carrito1")
+            .creationDate(LocalDateTime.now())
+            .lastUpdate(LocalDateTime.now())
+            .createdBy(Customer.builder()
+                    .id("juan")
+                    .userType("Customer")
+                    .username("juand")
+                    .firstName("juan")
+                    .lastName("echeverry")
+                    .email("juan_echeverry@javeriana.edu.co")
+                    .build())
+            .cartItems(List.of(
+                    CartItem.builder()
+                            .serviceId(1L)
+                            .quantity(2)
+                            .subtotal(200.0)
+                            .build(),
+                    CartItem.builder()
+                            .serviceId(2L)
+                            .quantity(1)
+                            .subtotal(150.0)
+                            .build()
+            ))
+            .build();
+
+    static final Cart carrito2 = Cart.builder()
+            .id("carrito2")
+            .creationDate(LocalDateTime.now())
+            .lastUpdate(LocalDateTime.now())
+            .createdBy(Customer.builder()
+                    .id("juan2")
+                    .userType("Customer")
+                    .username("juand2")
+                    .firstName("Juan")
+                    .lastName("echeverry")
+                    .email("juan_echeverry@javeriana.edu.co")
+                    .build())
+            .cartItems(List.of(
+                    CartItem.builder()
+                            .serviceId(3L)
+                            .quantity(3)
+                            .subtotal(300.0)
+                            .build(),
+                    CartItem.builder()
+                            .serviceId(4L)
+                            .quantity(2)
+                            .subtotal(250.0)
+                            .build()
+            ))
+            .build();
+
+    static void createMockKafkaConsumer() {
+        String groupId_1 = "service-group";
+        String groupId_2 = "service-type-group";
+        String groupId_3 = "my-consumer-group";
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId_1);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId_2);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId_3);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        properties.put(JsonDeserializer.TRUSTED_PACKAGES, "co.edu.javeriana.msc.*");
+        mockKafkaConsumer = new KafkaConsumer<>(properties, new JsonDeserializer<>(), new JsonDeserializer<>());
+        mockKafkaConsumer.subscribe(List.of("cartQueue"));
+    }
+
+    @BeforeAll
+    static void beforeAll(@Autowired CartRepository carritoRepository) {
+        kafka.start();
+        createMockKafkaConsumer();
+        mongo.start();
+        carritoRepository.save(carrito1);
+        carritoRepository.save(carrito2);
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mongo.stop();
+        kafka.stop();
+    }
+
+    @BeforeEach
+    void setup(@Autowired CartRepository carritoRepository) {
+        carritoRepository.deleteAll();
+        carritoRepository.save(carrito1);
+        carritoRepository.save(carrito2);
+    }
+
+
+    @Test
+    @Order(1)
+    void getCartTest(@Autowired CartRepository carritoRepository) {
+        CartResponse cartResponse = cartService.getCart(carrito1.getId());
+
+        assertThat(cartResponse).isNotNull();
+        assertThat(cartResponse.id()).isEqualTo(carrito1.getId());
+        assertThat(cartResponse.createdBy().getUsername()).isEqualTo(carrito1.getCreatedBy().getUsername());
     }
 
     @Test
-    void createCart() {
-        CartRequest cartRequest = createSampleCartRequest();
-        ResponseEntity<String> response = cartController.createCart(cartRequest);
+    @Order(2)
+    void deleteCartItemTest(@Autowired CartRepository carritoRepository) {
+        Long serviceIdToDelete = carrito1.getCartItems().get(0).getServiceId();
 
-        assertEquals(200, response.getStatusCodeValue());
-        assertNotNull(response.getBody());
+        cartService.deleteCartItem(carrito1.getId(), serviceIdToDelete);
+
+        var updatedCart = carritoRepository.findById(carrito1.getId()).orElseThrow();
+        assertThat(updatedCart.getCartItems().stream().noneMatch(item -> item.getServiceId().equals(serviceIdToDelete))).isTrue();
+    }
+
+
+    @Test
+    @Order(3)
+    void deleteCartTest(@Autowired CartRepository carritoRepository) {
+        cartService.deleteCart(carrito1.getId());
+
+        assertThat(carritoRepository.findById(carrito1.getId())).isEmpty();
     }
 
     @Test
-    void getCart() {
-        CartRequest cartRequest = createSampleCartRequest();
-        cartController.createCart(cartRequest);
+    @Order(4)
+    void getCartByUserTest() {
+        CartResponse cartResponse = cartService.getCartByUser(carrito1.getCreatedBy().getId());
 
-        ResponseEntity<CartResponse> response = cartController.getCart("carrito");
-
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals("carrito", response.getBody().id());
+        assertThat(cartResponse).isNotNull();
+        assertThat(cartResponse.createdBy().getId()).isEqualTo(carrito1.getCreatedBy().getId());
+        assertThat(cartResponse.cartItems()).hasSize(carrito1.getCartItems().size());
     }
 
     @Test
-    void getCartByUser() {
-        CartRequest cartRequest = createSampleCartRequest();
-        cartController.createCart(cartRequest);
+    @Order(5)
+    void purchaseTest() {
+        OrderPurchaseRequest purchaseRequest = cartService.toOrderRequest(cartService.getCart(carrito1.getId()));
 
-        ResponseEntity<CartResponse> response = cartController.getCartByUser("Juan");
-
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals("carrito", response.getBody().id());
+        assertThat(purchaseRequest).isNotNull();
+        assertThat(purchaseRequest.amount()).isGreaterThan(0);
     }
 
-    @Test
-    void deleteCart() {
-        CartRequest cartRequest = createSampleCartRequest();
-        cartController.createCart(cartRequest);
 
-        ResponseEntity<Void> response = cartController.deleteCart("carrito");
 
-        assertEquals(204, response.getStatusCodeValue());
-    }
 
-    @Test
-    void addCartItem() {
-        CartRequest cartRequest = createSampleCartRequest();
-        cartController.createCart(cartRequest);
-
-        CartItem newItem = new CartItem(2L, 1, 50.0);
-        ResponseEntity<String> response = cartController.addCartItem("carrito", newItem);
-
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals("Item añadido al carrito", response.getBody());
-    }
-
-    @Test
-    void deleteCartItem() {
-        CartRequest cartRequest = createSampleCartRequest();
-        cartController.createCart(cartRequest);
-
-        ResponseEntity<Void> response = cartController.deleteCartItem("carrito", 1L);
-
-        assertEquals(204, response.getStatusCodeValue());
-    }
-
-    @Test
-    void purchase() {
-        CartRequest cartRequest = createSampleCartRequest();
-        cartController.createCart(cartRequest);
-
-        ResponseEntity<OrderPurchaseRequest> response = cartController.purchase("carrito");
-
-        assertEquals(200, response.getStatusCodeValue());
-        assertNotNull(response.getBody());
-    }
 }
--- src/test/java/co/edu/javeriana/msc/turismo/order_management_microservice/orders/controllers/OrderPurchaseControllerTest.java
+++ src/test/java/co/edu/javeriana/msc/turismo/order_management_microservice/orders/controllers/OrderPurchaseControllerTest.java
@@ -1,121 +1,213 @@
 package co.edu.javeriana.msc.turismo.order_management_microservice.orders.controllers;
 
+import co.edu.javeriana.msc.turismo.order_management_microservice.dto.Customer;
+import co.edu.javeriana.msc.turismo.order_management_microservice.orders.Repository.OrderPurchaseRepository;
 import co.edu.javeriana.msc.turismo.order_management_microservice.orders.dto.OrderPurchaseRequest;
 import co.edu.javeriana.msc.turismo.order_management_microservice.orders.dto.OrderPurchaseResponse;
+import co.edu.javeriana.msc.turismo.order_management_microservice.orders.enums.PaymentStatus;
+import co.edu.javeriana.msc.turismo.order_management_microservice.orders.enums.Status;
+import co.edu.javeriana.msc.turismo.order_management_microservice.orders.model.OrderItem;
+import co.edu.javeriana.msc.turismo.order_management_microservice.orders.model.OrderPurchase;
 import co.edu.javeriana.msc.turismo.order_management_microservice.orders.services.OrderPurchaseService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureGraphQlTester
+@Testcontainers
+@TestMethodOrder(OrderAnnotation.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class OrderPurchaseControllerTest {
 
     @Autowired
-    private OrderPurchaseController orderPurchaseController;
-
-    @Autowired
     private OrderPurchaseService orderPurchaseService;
 
+    @Autowired
+    private OrderPurchaseRepository orderPurchaseRepository;
+
+    @Container
+    static final MongoDBContainer mongo = new MongoDBContainer("mongo:4.4.6");
+
+    @Container
+    static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.6.1"));
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.host", mongo::getHost);
+        registry.add("spring.data.mongodb.port", mongo::getFirstMappedPort);
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
+
+    static KafkaConsumer<Object, Object> mockKafkaConsumer;
+
+    static final OrderPurchase order1 = OrderPurchase.builder()
+            .id("1")
+            .creationDate(LocalDateTime.now())
+            .lastUpdate(LocalDateTime.now())
+            .orderStatus(Status.ACEPTADA)
+            .paymentStatus(PaymentStatus.ACEPTADA)
+            .createdBy(Customer.builder()
+                    .id("1")
+                    .userType("Customer")
+                    .username("juan")
+                    .firstName("juan")
+                    .lastName("juan")
+                    .email("juanjuan@juan.com")
+                    .build())
+            .orderItems(List.of(
+                    OrderItem.builder()
+                            .serviceId(1L)
+                            .quantity(2)
+                            .subtotal(100.0)
+                            .build(),
+                    OrderItem.builder()
+                            .serviceId(2L)
+                            .quantity(1)
+                            .subtotal(75.0)
+                            .build()
+            ))
+            .amount(175.0)
+            .build();
+
+    static final OrderPurchase order2 = OrderPurchase.builder()
+            .id("2")
+            .creationDate(LocalDateTime.now())
+            .lastUpdate(LocalDateTime.now())
+            .orderStatus(Status.RECHAZADA)
+            .paymentStatus(PaymentStatus.RECHAZADA)
+            .createdBy(Customer.builder()
+                    .id("2")
+                    .userType("Customer")
+                    .username("juand")
+                    .firstName("juan")
+                    .lastName("juan")
+                    .email("juanjuan@juan.com")
+                    .build())
+            .orderItems(List.of(
+                    OrderItem.builder()
+                            .serviceId(3L)
+                            .quantity(3)
+                            .subtotal(180.0)
+                            .build(),
+                    OrderItem.builder()
+                            .serviceId(4L)
+                            .quantity(2)
+                            .subtotal(80.0)
+                            .build()
+            ))
+            .amount(260.0)
+            .build();
+
+    static void createMockKafkaConsumer() {
+        String groupId_1 = "service-group";
+        String groupId_2 = "service-type-group";
+        String groupId_3 = "my-consumer-group";
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId_1);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId_2);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId_3);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        properties.put(JsonDeserializer.TRUSTED_PACKAGES, "co.edu.javeriana.msc.*");
+        mockKafkaConsumer = new KafkaConsumer<>(properties, new JsonDeserializer<>(), new JsonDeserializer<>());
+        mockKafkaConsumer.subscribe(List.of("cartQueue"));
+    }
+
+    @BeforeAll
+    static void beforeAll(@Autowired OrderPurchaseRepository orderRepository) {
+        kafka.start();
+        createMockKafkaConsumer();
+        mongo.start();
+        orderRepository.save(order1);
+        orderRepository.save(order2);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mongo.stop();
+        kafka.stop();
+    }
+
     @BeforeEach
-    void setUp() {
-
+    void setup(@Autowired OrderPurchaseRepository orderRepository) {
+        orderRepository.deleteAll();
+        orderRepository.save(order1);
+        orderRepository.save(order2);
     }
 
     @Test
-    void obtenerOrdenPorId() {
-        OrderPurchaseRequest request = new OrderPurchaseRequest("orden123", LocalDateTime.now(), LocalDateTime.now(), null, null, null, Collections.emptyList(), 100.0);
-        String createdOrderId = orderPurchaseService.createOrderPurchase(request);
+    @Order(1)
+    void getOrderByIdTest(@Autowired OrderPurchaseRepository orderRepository) {
+        OrderPurchaseResponse orderResponse = orderPurchaseService.findById(order1.getId());
 
-        ResponseEntity<OrderPurchaseResponse> result = orderPurchaseController.getOrder(createdOrderId);
-
-        assertEquals(200, result.getStatusCodeValue());
-        assertNotNull(result.getBody());
+        assertThat(orderResponse).isNotNull();
+        assertThat(orderResponse.id()).isEqualTo(order1.getId());
+        assertThat(orderResponse.createdBy().getUsername()).isEqualTo(order1.getCreatedBy().getUsername());
     }
 
     @Test
-    void obtenerTodasLasOrdenes() {
-        OrderPurchaseRequest request1 = new OrderPurchaseRequest("orden123", LocalDateTime.now(), LocalDateTime.now(), null, null, null, Collections.emptyList(), 100.0);
-        OrderPurchaseRequest request2 = new OrderPurchaseRequest("orden124", LocalDateTime.now(), LocalDateTime.now(), null, null, null, Collections.emptyList(), 150.0);
-        orderPurchaseService.createOrderPurchase(request1);
-        orderPurchaseService.createOrderPurchase(request2);
+    @Order(2)
+    void getAllOrdersTest() {
+        List<OrderPurchaseResponse> allOrders = orderPurchaseService.findAllOrderPurchases();
 
-        ResponseEntity<List<OrderPurchaseResponse>> result = orderPurchaseController.findAll();
-
-        assertEquals(200, result.getStatusCodeValue());
-        assertNotNull(result.getBody());
-        assertTrue(result.getBody().size() > 1);
+        assertThat(allOrders).isNotNull();
+        assertThat(allOrders.size()).isEqualTo(2); // Verifica que haya dos órdenes en el repositorio
+        assertThat(allOrders.get(0).id()).isEqualTo(order1.getId());
+        assertThat(allOrders.get(1).id()).isEqualTo(order2.getId());
     }
 
     @Test
-    void obtenerOrdenesAprobadasPorUsuario() {
-        String userId = "user123";
-        OrderPurchaseRequest request = new OrderPurchaseRequest("orden123", LocalDateTime.now(), LocalDateTime.now(), null, null, null, Collections.emptyList(), 100.0);
-        orderPurchaseService.createOrderPurchase(request);
+    @Order(3)
+    void getPurchasedByUserTest() {
+        List<OrderPurchaseResponse> purchasedOrders = orderPurchaseService.findAllPurchasedByUsers(order1.getCreatedBy().getId());
 
-        ResponseEntity<List<OrderPurchaseResponse>> result = orderPurchaseController.getPurchasedByUser(userId);
-
-        assertEquals(200, result.getStatusCodeValue());
-        assertNotNull(result.getBody());
-        assertTrue(result.getBody().size() >= 0);
+        assertThat(purchasedOrders).isNotNull();
+        assertThat(purchasedOrders).hasSize(1);
+        assertThat(purchasedOrders.get(0).id()).isEqualTo(order1.getId());
     }
 
     @Test
-    void obtenerOrdenesPorUsuario() {
-        String userId = "user123";
-        OrderPurchaseRequest request = new OrderPurchaseRequest("orden123", LocalDateTime.now(), LocalDateTime.now(), null, null, null, Collections.emptyList(), 100.0);
-        orderPurchaseService.createOrderPurchase(request);
+    @Order(4)
+    void getOrderedByUserTest() {
+        List<OrderPurchaseResponse> orderedOrders = orderPurchaseService.findAllOrdersByUsers(order2.getCreatedBy().getId());
 
-        ResponseEntity<List<OrderPurchaseResponse>> result = orderPurchaseController.getOrdersdByUser(userId);
-
-        assertEquals(200, result.getStatusCodeValue());
-        assertNotNull(result.getBody());
-        assertTrue(result.getBody().size() >= 0);
+        assertThat(orderedOrders).isNotNull();
+        assertThat(orderedOrders).hasSize(1);
+        assertThat(orderedOrders.get(0).id()).isEqualTo(order2.getId());
     }
+
 
     @Test
-    void crearNuevaOrden() {
-        OrderPurchaseRequest request = new OrderPurchaseRequest("orden123", LocalDateTime.now(), LocalDateTime.now(), null, null, null, Collections.emptyList(), 100.0);
+    @Order(5)
+    void deleteOrderTest() {
+        orderPurchaseService.deleteOrderPurchase(order1.getId());
 
-        ResponseEntity<String> result = orderPurchaseController.createOrder(request);
-
-        assertEquals(201, result.getStatusCodeValue());
-        assertNotNull(result.getBody());
-        assertEquals("orden123", result.getBody());
+        assertThat(orderPurchaseRepository.findById(order1.getId())).isEmpty();
     }
 
-    @Test
-    void actualizarEstadoDeOrden() {
-        String orderId = "orden123";
-        OrderPurchaseRequest request = new OrderPurchaseRequest(orderId, LocalDateTime.now(), LocalDateTime.now(), null, null, null, Collections.emptyList(), 100.0);
-        orderPurchaseService.createOrderPurchase(request);
 
-        OrderPurchaseRequest updateRequest = new OrderPurchaseRequest(orderId, LocalDateTime.now(), LocalDateTime.now(), null, null, null, Collections.emptyList(), 120.0);
 
-        ResponseEntity<OrderPurchaseResponse> result = orderPurchaseController.updateOrderEstado(orderId, updateRequest);
-
-        assertEquals(200, result.getStatusCodeValue());
-        assertNotNull(result.getBody());
-    }
-
-    @Test
-    void eliminarOrden() {
-        String orderId = "orden123";
-        OrderPurchaseRequest request = new OrderPurchaseRequest(orderId, LocalDateTime.now(), LocalDateTime.now(), null, null, null, Collections.emptyList(), 100.0);
-        orderPurchaseService.createOrderPurchase(request);
-
-        ResponseEntity<Void> result = orderPurchaseController.deleteOrder(orderId);
-
-        assertEquals(204, result.getStatusCodeValue());
-
-    }
 }
